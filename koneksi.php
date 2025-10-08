6ba96f9b467a2fa7259b2c65d3985b432d78f408
reipand <reisanadrefagt@gmail.com>
2025-10-08 21:35:25 +0700

    update koneksi dengan docker

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,17 @@
 <?php
-$host = "localhost";      // Server database
-$user = "reip";           // Username default XAMPP
-$pass = "bcst2526";               // Password default (kosong)
-$db   = "jeep"; // Nama database kamu
+
+$host = "db";      
+$user = "jeep_user";           
+$pass = "bcst2526";               
+$db   = "jeep_db"; 
 
 // Membuat koneksi
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 // Mengecek koneksi
 if (!$conn) {
+    // Penting: Di lingkungan development, tampilkan error.
+    // Di lingkungan produksi, log error ini, jangan tampilkan ke user.
     die("Koneksi gagal: " . mysqli_connect_error());
 } else {
     // echo "Koneksi berhasil"; // aktifkan jika ingin cek koneksi
